5760095fc5941eaaff7c7919c3aff71d999cb8a0
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2021-01-29 14:50:25 -0500

    Start working on the MyProjects section.

--- js/interactions.js
+++ js/interactions.js
@@ -4,7 +4,7 @@ $(window).on("load",function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
       $(".fade").each(function() {
         /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).innerHeight()/4;
+        var objectBottom = $(this).offset().top + $(this).innerHeight()/2;
 
         /* If the element is completely within bounds of the window, fade it in */
         if (objectBottom < windowBottom) { //object comes into view (scrolling down)
