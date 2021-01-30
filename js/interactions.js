0c4694b897d2f6c7ab397322359cc3e76c4d5e17
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2021-01-29 22:32:48 -0500

    Make drop down project animations function smoothly.

--- js/interactions.js
+++ js/interactions.js
@@ -1,21 +1,32 @@
 //Fade-In Animation for components as they come into the view of the window
 $(window).on("load",function() {
-    $(window).scroll(function() {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".fade").each(function() {
-        /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).innerHeight()/2;
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).innerHeight()/2;
 
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-          if (!$(this).hasClass("inView")) {
-            $(this).addClass("inView");
-          }
-        } else { //object goes out of view (scrolling up)
-          if ($(this).hasClass("inView")) {
-            $(this).removeClass("inView");
-          }
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if (!$(this).hasClass("inView")) {
+          $(this).addClass("inView");
         }
-      });
-    }).scroll(); //invoke scroll-handler on page-load
+      } else { //object goes out of view (scrolling up)
+        if ($(this).hasClass("inView")) {
+          $(this).removeClass("inView");
+        }
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
   });
+
+$(document).ready(function() {
+  $(".dropdownArrow").click(function() {
+    const parent = $(this).parents(".accordian");
+    if(parent.hasClass("isActive")) {
+      parent.removeClass("isActive");
+    } else {
+      parent.addClass("isActive");
+    }
+  });
+});
