3bac8a3fd9ac577b1a166289b864cbc4a0e1cf10
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2021-01-31 00:08:36 -0500

    Start working on the Let's Connect section of website.

--- js/interactions.js
+++ js/interactions.js
@@ -4,7 +4,7 @@ $(window).on("load",function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade").each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).innerHeight()/2;
+      var objectBottom = $(this).offset().top + $(this).innerHeight()/3;
 
       /* If the element is completely within bounds of the window, fade it in */
       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
@@ -20,7 +20,10 @@ $(window).on("load",function() {
   }).scroll(); //invoke scroll-handler on page-load
   });
 
+
 $(document).ready(function() {
+
+  //Add drop down arrow interactions
   $(".dropdownArrow").click(function() {
     const parent = $(this).parents(".accordian");
     if(parent.hasClass("isActive")) {
@@ -29,4 +32,9 @@ $(document).ready(function() {
       parent.addClass("isActive");
     }
   });
+
+  $(".contact > div > img").click(function() {
+    console.log("TEST");
+  })
+
 });
